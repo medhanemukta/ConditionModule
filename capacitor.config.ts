e1881d4be4d5f3c2951c7cbb46539ec59ec9b2ca
mukta <mukta.medhane@triarqhealth.com>
2022-06-16 15:02:00 +0530

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'condition module',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
